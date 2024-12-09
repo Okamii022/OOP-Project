1d86d3dc11d5df29710f51efc2c1e6eb39798326
Neagu Andrei <raresneagu34@gmail.com>
2024-12-09 23:06:28 +0200

    Constructor and destructor added

--- Project/COLUMN.h
+++ Project/COLUMN.h
@@ -11,5 +11,14 @@ private:
 	std::string defaultValue = ""; // default value if no value is added
 	bool isPrimaryKey = false; //is the column a primary key or not
 	bool isForeignKey = false; //is the column a foreign key or not
+public:
+	//parameterized ctor using initializer list
+	Column (const std::string columnName, const std::string columnType, const int size, 
+		const std::string defaultValue, const bool isPrimaryKey, const bool isForeignKey)
+		:columnName(columnName), columnType(columnType), size(size), defaultValue(defaultValue),
+		isPrimaryKey(isPrimaryKey), isForeignKey(isForeignKey){ }
+	//destructor
+	~Column() {
 
+	}
 };
