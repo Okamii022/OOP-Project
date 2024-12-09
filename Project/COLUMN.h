e54e91299369d9c06080169b641da32077e81d54
Neagu Andrei <raresneagu34@gmail.com>
2024-12-09 22:20:36 +0200

    Added further comments to explain the attributes

--- Project/COLUMN.h
+++ Project/COLUMN.h
@@ -7,10 +7,9 @@ class Column {
 private:
 	std::string columnName = ""; //name of the column
 	std::string columnType = ""; // type of the column, integer, float or text
-	int size = 0;
-	std::string defaultValue = "";
-	bool isPrimaryKey = false;
-	bool isForeignKey = false;
-
+	int size = 0; //the size of the data associated to each column
+	std::string defaultValue = ""; // default value if no value is added
+	bool isPrimaryKey = false; //is the column a primary key or not
+	bool isForeignKey = false; //is the column a foreign key or not
 
 };
