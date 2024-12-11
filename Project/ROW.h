33f6ca7cc6e778bcf92f1e9aea1b63bcebe2c967
Neagu Andrei <raresneagu34@gmail.com>
2024-12-11 22:48:57 +0200

    Added a Destructor and two possible constructors for this class

--- Project/ROW.h
+++ Project/ROW.h
@@ -9,6 +9,13 @@ private:
 	int columnNumber; //will be used for the dynamic array
 
 public:
-
-
+	Row(int rowNumber, std::string value, int columnNumber) :rowNumber(rowNumber), columnNumber(columnNumber)
+	{
+		//copy for dynamic field to be added (does it belong here though?)
+	}
+	Row(int colCount)
+	~Row()
+	{
+		delete[] value;
+	}
 };
