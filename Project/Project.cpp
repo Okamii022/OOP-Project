09401d45f5bb8a52fb7fd720b2009d9a92e1e7a4
Neagu Andrei <raresneagu34@gmail.com>
2024-12-16 00:08:04 +0200

    Added the setters and getters

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -1,13 +1,18 @@
-//#include "COLUMN.h"
+#include "COLUMN.h"
 #include "ROW.h"
 #include <iostream>
 
 int main()
 {
     string test[] = { "key","board","ceva"};
+    string test2[] = { "key","board","boom"};
     int noCol = 3;
     int row = 1;
-    Row rowEntry(row, test, noCol);
+    Row rowEntry(test, noCol);
     std::cout << rowEntry;
+    Row rowEntry2(test2,noCol);
+    std::cout << rowEntry2;
 }
+
+
 
